6b88066d02f2cdd1be3611e9ba485af593bc4810
dmmejia <danny.mejia@ceiba.com.co>
2022-07-29 07:55:39 -0500

    implementacion config server

---
+++ servicio-configuracion/src/test/java/com/ceiba/biblioteca/configuracion/ServicioConfiguracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceiba.biblioteca.configuracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioConfiguracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
